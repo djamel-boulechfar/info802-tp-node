396c969dab6fc5db929d2f69d2d4d22ec53c4364
Djamel <djamelboulechfar@gmail.com>
2022-02-28 17:53:08 +0100

    Temps parcours

--- public/main.js
+++ public/main.js
@@ -1,16 +1,66 @@
 const socket = io();
 
+window.onload = function() {
+    recupererListeVoitures();
+
+    document.querySelector("#formulaireParcours").addEventListener("submit", (e) => {
+        e.preventDefault();
+        var selectListeVoitures = document.getElementById("selectListeVoitures");
+        var nomVoiture = selectListeVoitures.options[selectListeVoitures.selectedIndex].value;
+        if (nomVoiture === "none") {
+            alert("ERREUR: Veuillez d'abord sélectionner une voiture.");
+        } else {
+            var distance = document.getElementById("inputDistance").value;
+            var autonomie = document.getElementById("inputAutonomie").value;
+            var tempsDeRecharge = document.getElementById("inputTempsDeRecharge").value;
+            var vitesseMoyenne = document.getElementById("inputVitesseMoyenne").value;
+            socket.emit("CalculeTempsParcours", distance, autonomie, tempsDeRecharge, vitesseMoyenne);
+        }
+    });
+}
+
 function recupererListeVoitures() {
     socket.emit("RecupererListeVoitures");
 }
 
+function recupererInfosVoiture() {
+    var selectListeVoitures = document.getElementById("selectListeVoitures");
+    var nomVoiture = selectListeVoitures.options[selectListeVoitures.selectedIndex].value;
+    socket.emit("RecupererInfosVoiture", nomVoiture);
+}
+
 socket.on("ListeVoitures", function(voitures) {
-    const tableauVoitures = JSON.parse(voitures);
-    const ulListeVoitures = document.getElementById("ulListeVoitures");
-    ulListeVoitures.innerHTML = "";
+    var tableauVoitures = JSON.parse(voitures);
+    var selectListeVoitures = document.getElementById("selectListeVoitures");
     for (let i = 0; i < tableauVoitures.length; i++) {
-        var liVoiture = document.createElement("li");
-        liVoiture.innerHTML = tableauVoitures[i].nom + " (autonomie : " + tableauVoitures[i].autonomie + "km, temps de rechargement : " + tableauVoitures[i].tempsDeRecharge + "h)";
-        ulListeVoitures.appendChild(liVoiture);
+        var optionVoiture = document.createElement("option");
+        optionVoiture.setAttribute("value", tableauVoitures[i].nom);
+        optionVoiture.innerHTML = tableauVoitures[i].nom;
+        selectListeVoitures.appendChild(optionVoiture);
     }
+});
+
+socket.on("InfosVoiture", function(nomVoiture, voitures) {
+    var tableauVoitures = JSON.parse(voitures);
+    var inputAutonomie = document.getElementById("inputAutonomie");
+    var inputTempsDeRecharge = document.getElementById("inputTempsDeRecharge");
+    for (let i = 0; i < tableauVoitures.length; i++) {
+        if (tableauVoitures[i].nom === nomVoiture) {
+            inputAutonomie.setAttribute("value", tableauVoitures[i].autonomie);
+            inputTempsDeRecharge.setAttribute("value", tableauVoitures[i].tempsDeRecharge);
+        }
+    } 
+});
+
+socket.on("ReponseTempsParcours", function(response) {
+    var tempsParcours = JSON.parse(response).tempsParcours;
+    var pReponseTempsParcours = document.getElementById("pReponseTempsParcours");
+    var heures = Math.floor(tempsParcours / 60);
+    var minutes = tempsParcours % 60;
+    if (minutes != 0) {
+        pReponseTempsParcours.innerHTML = "Temps de parcours : " + heures + " heures et " + minutes + " minutes.";
+    } else {
+        pReponseTempsParcours.innerHTML = "Temps de parcours : " + heures + " heures.";
+    }
+    
 });
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(http);
 const soap = require("soap");
+const request = require('request');
 
-// const url = 'http://localhost:8000/?wsdl';
-const url = "https://info802tp-soap-python.herokuapp.com/?wsdl";
+//const urlSoap = 'http://localhost:8000/?wsdl';
+const urlSoap = "https://info802tp-soap-python.herokuapp.com/?wsdl";
+
+//const urlRest = 'http://localhost:5000';
+const urlRest = 'https://info802tp-rest-python.herokuapp.com/';
 
 app.use(require("express").static("public"));
 
@@ -14,18 +18,38 @@ app.get('/', function(req, res) {
 });
    
 http.listen(port, function() { 
-    console.log('listening on *:' + port); 
+    console.log('Server listening on port ' + port); 
 });
 
 io.on("connect", function(socket) {
     socket.on("RecupererListeVoitures", function(){
-        soap.createClient(url, function(err, client) {
+        soap.createClient(urlSoap, function(err, client) {
             if (err) throw err;
             var args = {};
             client.recupererListeVoitures(args, function(err, result) {
                 if (err) throw err;
                 io.emit("ListeVoitures", result.recupererListeVoituresResult);
             });
+        });
+    });
+
+    socket.on("RecupererInfosVoiture", function(nomVoiture) {
+        soap.createClient(urlSoap, function(err, client) {
+            if (err) throw err;
+            var args = {};
+            client.recupererListeVoitures(args, function(err, result) {
+                if (err) throw err;
+                io.emit("InfosVoiture", nomVoiture, result.recupererListeVoituresResult);
+            });
+        });
+    });
+
+    socket.on("CalculeTempsParcours", function(distance, autonomie, tempsDeRecharge, vitesseMoyenne) {
+        request(urlRest + '/calculeTempsParcours/' + distance + '/' + autonomie + '/' + tempsDeRecharge + '/' + vitesseMoyenne, function(error, response) {
+            console.error('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log("Temps : " + response.body);
+            io.emit("ReponseTempsParcours", response.body);
         });
     });
 });
