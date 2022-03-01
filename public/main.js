4fc1d8bb29e45612a03828824ae31c6156cb5d67
Djamel <djamelboulechfar@gmail.com>
2022-03-01 11:21:01 +0100

    Commentaires

--- public/main.js
+++ public/main.js
@@ -1,12 +1,14 @@
 const socket = io();
 
+// Chargement de la liste des voitures et assignation d'un event pour calculer la durée du trajet (avec ma propre API, sans Google Maps, partie de gauche du site)
 window.onload = function() {
-    recupererListeVoitures();
+    recupererListeVoitures(); // Récupération de la liste des voitures
 
+    // Ajout du listener sur le submit du formulaire (partie gauche du site)
     document.querySelector("#formulaireParcours").addEventListener("submit", (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Annulation de l'effet de base de l'event submit
         var selectListeVoitures = document.getElementById("selectListeVoitures");
-        var nomVoiture = selectListeVoitures.options[selectListeVoitures.selectedIndex].value;
+        var nomVoiture = selectListeVoitures.options[selectListeVoitures.selectedIndex].value; // Récupération du nom de la voiture sélectionnée
         if (nomVoiture === "none") {
             alert("ERREUR: Veuillez d'abord sélectionner une voiture.");
         } else {
@@ -14,25 +16,29 @@ window.onload = function() {
             var autonomie = document.getElementById("inputAutonomie").value;
             var tempsDeRecharge = document.getElementById("inputTempsDeRecharge").value;
             var vitesseMoyenne = document.getElementById("inputVitesseMoyenne").value;
+            // Emission de l'event permettant de demander au serveur de calculer la durée du trajet grâce à mon API Rest (sans Google Maps)
             socket.emit("CalculeTempsParcours", distance, autonomie, tempsDeRecharge, vitesseMoyenne);
         }
     });
 }
 
+// Fonction émettant sur la socket l'event permettant de demander au serveur la liste des véhicules disponibles grâce à mon service Soap
 function recupererListeVoitures() {
     socket.emit("RecupererListeVoitures");
 }
 
+// Fonction émettant sur la socket l'event permettant de demander au serveur les informations relatives au véhicule sélectionné grâce à mon service Soap
 function recupererInfosVoiture() {
     var selectListeVoitures = document.getElementById("selectListeVoitures");
     var nomVoiture = selectListeVoitures.options[selectListeVoitures.selectedIndex].value;
     socket.emit("RecupererInfosVoiture", nomVoiture);
 }
 
+// Ecoute l'event sur lequel le serveur envoie la liste des véhicules récupérés grâce à mon service Soap
 socket.on("ListeVoitures", function(voitures) {
-    var tableauVoitures = JSON.parse(voitures);
+    var tableauVoitures = JSON.parse(voitures); // Parsing de la chaîne de caractères reçue en JSON
     var selectListeVoitures = document.getElementById("selectListeVoitures");
-    for (let i = 0; i < tableauVoitures.length; i++) {
+    for (let i = 0; i < tableauVoitures.length; i++) { // Ajout d'une option dans le select pour chaque voiture
         var optionVoiture = document.createElement("option");
         optionVoiture.setAttribute("value", tableauVoitures[i].nom);
         optionVoiture.innerHTML = tableauVoitures[i].nom;
@@ -40,46 +46,52 @@ socket.on("ListeVoitures", function(voitures) {
     }
 });
 
+// Ecoute l'event sur lequel le serveur envoie les informations du véhicule sélectionné récupérées grâce à mon service Soap
 socket.on("InfosVoiture", function(nomVoiture, voitures) {
-    var tableauVoitures = JSON.parse(voitures);
+    var tableauVoitures = JSON.parse(voitures); // Parsing de la chaîne de caractères en JSON
     var inputAutonomie = document.getElementById("inputAutonomie");
     var inputTempsDeRecharge = document.getElementById("inputTempsDeRecharge");
     for (let i = 0; i < tableauVoitures.length; i++) {
-        if (tableauVoitures[i].nom === nomVoiture) {
+        if (tableauVoitures[i].nom === nomVoiture) { // Affichage des informations si le nom du véhicule correspond au nom du véhicule sélectionné
             inputAutonomie.setAttribute("value", tableauVoitures[i].autonomie);
             inputTempsDeRecharge.setAttribute("value", tableauVoitures[i].tempsDeRecharge);
         }
     } 
 });
 
+// Ecoute l'event sur lequel le serveur envoie le temps de parcours calculé grâce à mon API Rest (sans Google Maps)
 socket.on("ReponseTempsParcours", function(response) {
     var tempsParcours = JSON.parse(response).tempsParcours;
     var pReponseTempsParcours = document.getElementById("pReponseTempsParcours");
+    // Calcul du temps de trajet en heures et minutes
     var heures = Math.floor(tempsParcours / 60);
     var minutes = Math.floor(tempsParcours % 60);
-    if (minutes != 0) {
+    if (minutes != 0) { // Affichage du temps de trajet sur la page web
         pReponseTempsParcours.innerHTML = "Temps de parcours : " + heures + " heures et " + minutes + " minutes.";
     } else {
         pReponseTempsParcours.innerHTML = "Temps de parcours : " + heures + " heures.";
     }
 });
 
-// Partie pour Google Maps
+// Fonction appelée pour afficher la map grâce à l'API de Google Maps
 function initMap() {
+    // Coordonnées de départ pour l'affichage de la map
     const latitudeLongitudeDepart = {
         lat: 45.649392,
         lng: 5.859076
     };
     
+    // Options de départ pour l'affichage de la map
     const mapOptions = {
         center: latitudeLongitudeDepart,
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     
+    // Instanciation de la map dans la div créée pour la contenir
     const map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
-    // Direction service
+    // DirectionsService
     const directionsService = new google.maps.DirectionsService();
 
     // DirectionsRenderer
@@ -88,59 +100,55 @@ function initMap() {
     // Liaison du renderer à la map
     directionsRenderer.setMap(map);
 
-    // Listener sur le bouton "Calculer le trajet"
+    // Ajout d'un listener sur le bouton "Calculer le trajet" pour afficher le trajet entre deux villes données sur la carte,
+    // calculer la distance de ce trajet et sa durée et demander au serveur de faire appel à mon API Rest pour ajouter le temps
+    // de recharge nécessaire en fonction de l'autonomie du véhicule et de la distance du trajet à la durée de base du trajet
     document.querySelector("#formulaireGoogleMaps").addEventListener("submit", (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Annulation de l'effet de base de l'event submit
         var selectListeVoitures = document.getElementById("selectListeVoitures");
         var nomVoiture = selectListeVoitures.options[selectListeVoitures.selectedIndex].value;
         if (nomVoiture === "none") {
             alert("ERREUR: Veuillez d'abord sélectionner une voiture.");
         } else {
+            // Récupération de la ville de départ et de la ville d'arrivée
             var depart = document.getElementById("inputDepart").value;
             var arrivee = document.getElementById("inputArrivee").value;
     
-            // Requête pour obtenir le chemin entre deux villes
+            // Requête pour obtenir le chemin entre deux villes en voiture
             var request = {
                 origin: depart,
                 destination: arrivee,
                 travelMode: google.maps.TravelMode.DRIVING,
                 unitSystem: google.maps.UnitSystem.METRIC
             }
-    
+            
+            // Execution de la requête avec les services de Google
             directionsService.route(request, function(result, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
+                if (status == google.maps.DirectionsStatus.OK) { // Si la requête a fonctionné
+                    // Récupération des informations du trajet
                     var distanceParcours = Math.floor(result.routes[0].legs[0].distance.value / 1000);
                     var dureeParcours = result.routes[0].legs[0].duration.value;
                     var autonomie = document.getElementById("inputAutonomie").value;
                     var tempsDeRecharge = document.getElementById("inputTempsDeRecharge").value;
-                    distanceTrajet.innerHTML = distanceParcours + " km";
-                    //dureeTrajet.innerHTML = dureeParcours;
-                    console.log(result.routes[0].legs[0].duration.text);
-                    directionsRenderer.setDirections(result);
+                    distanceTrajet.innerHTML = result.routes[0].legs[0].distance.text; // Affichage de la distance du trajet
+                    directionsRenderer.setDirections(result); // Affichage du tracé sur la map
+                    // Emission de l'event permettant de demander au serveur de calculer la durée du trajet grâce à mon API Rest (en prenant en compte Google Maps)
                     socket.emit("CalculeTempsParcoursAvecGoogleMaps", distanceParcours, autonomie, tempsDeRecharge, dureeParcours);
-                } else {
+                } else { // Si la requête a échoué
                     alert("Erreur sur le calcul du trajet, veuillez vérifier les informations entrées. Il se peut également que le trajet demandé ne soit pas réalisable.")
                 }
             });
         }
     });
-
-    // Autocomplétion nom des villes
-    var options = {
-        types: ['(cities)']
-    }
-
-    var inputDepart = document.getElementById("inputDepart");
-    var autocomplete1 = new google.maps.places.Autocomplete(inputDepart, options);
-
-    var inputArrivee = document.getElementById("inputArrivee");
-    var autocomplete2 = new google.maps.places.Autocomplete(inputArrivee, options);
 }
 
+// Ecoute l'event sur lequel le serveur envoie le temps de parcours calculé grâce à mon API Rest (en prenant en compte Google Maps)
 socket.on("ReponseTempsParcoursAvecGoogleMaps", function(resultat) {
-    var infosTempsParcours = JSON.parse(resultat);
+    var infosTempsParcours = JSON.parse(resultat); // Parsing de la chaîne de caractères reçue en JSON
     var elementTempsParcours = document.getElementById("dureeTrajet");
+    // Calcule de la durée en heures et minutes
     var heures = Math.floor(infosTempsParcours.tempsParcours / 3600);
     var minutes = Math.floor((infosTempsParcours.tempsParcours % 3600) / 60);
+    // Affichage du temps de parcours sur le site
     elementTempsParcours.innerHTML = heures + " heure(s) et " + minutes + " minute(s) dont " + infosTempsParcours.nbRecharges + " recharge(s) de " + infosTempsParcours.tempsChargement + " minute(s) chacune";
 });
--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const io = require("socket.io")(http);
 const soap = require("soap");
 const request = require('request');
 
-//const urlSoap = 'http://localhost:8000/?wsdl';
 const urlSoap = "https://info802tp-soap-python.herokuapp.com/?wsdl";
 
-//const urlRest = 'http://localhost:5000';
 const urlRest = 'https://info802tp-rest-python.herokuapp.com';
 
 app.use(require("express").static("public"));
@@ -22,6 +20,9 @@ http.listen(port, function() {
 });
 
 io.on("connect", function(socket) {
+
+    // Envoie une requête à mon service Soap pour récupérer la liste des véhicules disponibles
+    // et renvoie cette liste au fichier js lié à ma page HTML
     socket.on("RecupererListeVoitures", function(){
         soap.createClient(urlSoap, function(err, client) {
             if (err) throw err;
@@ -33,6 +34,8 @@ io.on("connect", function(socket) {
         });
     });
 
+    // Envoie une requête à mon service Soap pour récupérer les informations de la voiture
+    // dont le nom est passé en paramètre et renvoie ces informations au fichier js lié à ma page HTML
     socket.on("RecupererInfosVoiture", function(nomVoiture) {
         soap.createClient(urlSoap, function(err, client) {
             if (err) throw err;
@@ -44,6 +47,8 @@ io.on("connect", function(socket) {
         });
     });
 
+    // Envoie une requête à mon API Rest pour calculer un temps de parcours en fonction des paramètres donnés
+    // et renvoie ce temps de parcours au fichier js lié à ma page HTML
     socket.on("CalculeTempsParcours", function(distance, autonomie, tempsDeRecharge, vitesseMoyenne) {
         request(urlRest + '/calculeTempsParcours/' + distance + '/' + autonomie + '/' + tempsDeRecharge + '/' + vitesseMoyenne, function(error, response) {
             console.error('error:', error); // Print the error if one occurred
@@ -53,6 +58,9 @@ io.on("connect", function(socket) {
         });
     });
 
+    // Envoie une requête à mon API Rest pour calculer un temps de parcours en fonction du temps de trajet calculé
+    // par Google Maps et du temps de recharge nécessaire en fonction de l'autonomie du véhicule sélectionné et de la distance du parcours
+    // et renvoie ce temps de parcours au fichier js lié à ma page HTML
     socket.on("CalculeTempsParcoursAvecGoogleMaps", function(distanceParcours, autonomie, tempsDeRecharge, dureeParcours) {
         request(urlRest + '/calculeTempsParcoursAvecMaps/' + distanceParcours + '/' + autonomie + '/' + tempsDeRecharge + '/' + dureeParcours, function(error, response) {
             console.error('error:', error); // Print the error if one occurred
